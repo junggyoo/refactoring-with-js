5fa41cdcd474a678b50066a60881807b32cdc6a4
junggyoo <jungkyu.bae@meshkorea.net>
2022-09-14 17:45:25 +0900

    refactor: 레코드 캡슐화하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,31 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  #data;
 
-organization.name = 'Dream Coding';
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get country() {
+    return this.#country;
+  }
+
+  get rawData() {
+    return { ...this.#data }; // 얕은 복사
+  }
+}
+
+const organization = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB',
+});
+
 console.log(organization.name);
 console.log(organization.country);

e37216ba05f7a9d6bd0d10cb26d84b59210840b8
junggyoo <jungkyu.bae@meshkorea.net>
2022-09-20 18:22:43 +0900

    refactor: 7-9

--- ch7/7-9.js
+++ ch7/7-9.js
@@ -1,16 +1,6 @@
 function foundPerson(people) {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') {
-      return 'Don';
-    }
-    if (people[i] === 'John') {
-      return 'John';
-    }
-    if (people[i] === 'Kent') {
-      return 'Kent';
-    }
-  }
-  return '';
+  const caditates = ['Don', 'John', 'Kent'];
+  return people.find((p) => caditates.includes(p)) || '';
 }
 
 console.log(foundPerson(['John']));
